28ca2ec9fa9cf6f6f4e4dd626477d91a9c847632
kimminjun9 <kimminjun9@worksmobile.com>
2023-01-04 15:26:25 +0900

    [chore] 프로젝트 생성

---
+++ CombineAPITutorial/CombineAPITutorial/CombineAPITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAPITutorialApp.swift
+//  CombineAPITutorial
+//
+//  Created by USER on 2023/01/04.
+//
+
+import SwiftUI
+
+@main
+struct CombineAPITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
