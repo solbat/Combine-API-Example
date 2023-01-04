40ed34bea2e8abeb307a72401ea9b0fe499a372d
kimminjun9 <kimminjun9@worksmobile.com>
2023-01-04 15:44:49 +0900

    [feat] APIService enum 생성

--- CombineAPITutorial/CombineAPITutorial/APIService.swift
+++ CombineAPITutorial/CombineAPITutorial/APIService.swift
@@ -22,3 +22,20 @@ enum API {
     }
 }
 
+// struct로 할 수도 있음
+enum APIService {
+    // static : 객체 인스턴스를 만들지 않아도 함수를 호출할 수 있음
+    static func fetchTodos() -> AnyPublisher<[Todo], Error> {
+        return URLSession.shared.dataTaskPublisher(for: API.fetchTodos.url)
+            .map { $0.data }
+            .decode(type: [Todo].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+    static func fetchPosts() -> AnyPublisher<[Post], Error> {
+        return URLSession.shared.dataTaskPublisher(for: API.fetchPosts.url)
+            .map { $0.data }
+            .decode(type: [Post].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
