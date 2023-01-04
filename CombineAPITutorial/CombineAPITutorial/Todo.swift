39145faf42624e6e465b6a6c145e7a0fd82117e3
kimminjun9 <kimminjun9@worksmobile.com>
2023-01-04 15:45:25 +0900

    [feat] Todo 구조체 생성

---
+++ CombineAPITutorial/CombineAPITutorial/Todo.swift
@@ -0,0 +1,20 @@
+//
+//  Todo.swift
+//  CombineAPITutorial
+//
+//  Created by USER on 2023/01/04.
+//
+
+import Foundation
+
+// MARK: - Todo
+struct Todo: Codable {
+    let userID, id: Int
+    let title: String
+    let completed: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case userID = "userId"
+        case id, title, completed
+    }
+}
